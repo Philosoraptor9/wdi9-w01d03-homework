250d4aef92a81ab9050ba593b47e32829af90ac0
Philip Hoover <philhoov90@gmail.com>
2018-09-12 23:58:57 -0600

    Getting to know you answered

--- javascript/app.js
+++ javascript/app.js
@@ -25,19 +25,52 @@
 
 // FIZZBUZZ
 
-for (let i = 1; i <= 100; i++){
-    if(i % 3 === 0 && i % 5 !== 0){
-    console.log('Fizz');
-    } else if 
-    (i % 5 === 0 && i % 3 !== 0){
-    console.log('Buzz');    
-    } else if
-    (i % 3 === 0 && i % 5 === 0){
-    console.log('FizzBuzz');
-    } else {
-    console.log(i);
-    }
-}
+// for (let i = 1; i <= 100; i++){
+//     if(i % 3 === 0 && i % 5 !== 0){
+//     console.log('Fizz');
+//     } else if 
+//     (i % 5 === 0 && i % 3 !== 0){
+//     console.log('Buzz');    
+//     } else if
+//     (i % 3 === 0 && i % 5 === 0){
+//     console.log('FizzBuzz');
+//     } else {
+//     console.log(i);
+//     }
+// }
 
 
+// GETTING TO KNOW YOU
+
+const shahzad = ["Shahzad", 1000, "Austin"];
+const jim = ["Jim", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const daniel = ["Daniel", 186, "Dallas"];
+const ryan = ["Ryan", 65, "Denver"];
+
+// 1.
+shahzad.splice(0, 1, "Gameboy");
+console.log(shahzad);
+
+// 2. 
+daniel.splice(1, 1, daniel[1] + 1);
+console.log(daniel);
+
+// 3.
+ryan.splice(2, 1, "Gotham");
+console.log(ryan);
+
+// 4.
+reuben.pop()
+reuben.push("Chicago");
+console.log(reuben);
+
+// 5.
+jim.pop();
+jim.push("Albequerque", "Tuscon", "Salt Lake City");
+console.log(jim);
+
+// 6.
+jim.splice(2, 1)
+console.log(jim);
 
