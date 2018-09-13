d9004ccab579e0d96110d90f7dfb6035b907b381
Philip Hoover <philhoov90@gmail.com>
2018-09-12 23:52:50 -0600

    Commit 3 - Excited Kittens answered sort of

--- javascript/app.js
+++ javascript/app.js
@@ -3,8 +3,25 @@
 // console.log(i);
 // }
 
-for(let i = 0; i <= 200; i++){
-    if (i % 2 === 0){
-        console.log(i);
-    }
+// for(let i = 0; i <= 200; i++){
+//     if (i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+// EXCITED KITTEN
+
+const randomCat = ["...human...why you taking pictures of me?...", "...the catnip made me do it...",  "...why does the red dot always get away..."]
+let randomPhrase = Math.floor(Math.random() * randomCat.length);
+
+for(let i = 1; i <= 20; i++){
+    if (i % 2 === 0){ 
+    console.log(randomCat[randomPhrase]);
+    } else {
+    console.log("Love me, pet me! HSSSSSS!"); 
 }
+}
+
+
+
